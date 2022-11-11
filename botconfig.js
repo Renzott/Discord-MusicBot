3854b263b21d3abf55fcfceb3a8060643af2b208
root <root@Ubuntu-Win11>
2022-11-11 00:32:48 -0500

    feat: edit pass

--- botconfig.js
+++ botconfig.js
@@ -34,9 +34,9 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "localhost", //- The host name or IP of the lavalink server.
+    host: "lavalink", //- The host name or IP of the lavalink server.
     port: 2333, // The port that lavalink is listening to. This must be a number!
-    pass: "bonsoirDocker", //- The password of the lavalink server.
+    pass: "betterPassword", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
